566637672c4fb90c5a6ebe09fdb9aaa06fb04aad
Orintaa <orinta.strac@gmail.com>
2022-04-24 16:02:32 +0300

    init static app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from './Card';
+
+const ExpenseItem = (props) => {
+
+    return <Card className='expense-item'>
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount} Eur</div>
+        </div>
+        <button>Change Title</button>
+    </Card>;
+}
+
+export default ExpenseItem;
